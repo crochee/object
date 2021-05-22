420202c0bd3b15fd8fd5e4f771f9904bfb705f68
crochee <2843065113@qq.com>
2021-05-22 22:41:11 +0800

    fix test

--- pkg/rs/reed_solomon_test.go
+++ pkg/rs/reed_solomon_test.go
@@ -16,8 +16,7 @@ func TestRS(t *testing.T) {
 	data, err := os.ReadFile("../../test/test_rs.txt")
 	require.NoError(t, err)
 	t.Logf("%s", data)
-	dataShards := 6
-	enc, err := reedsolomon.New(dataShards, len(data)/dataShards)
+	enc, err := reedsolomon.New(2, 3)
 	require.NoError(t, err)
 	// Split the data into shards
 	shards, err := enc.Split(data)
@@ -29,20 +28,31 @@ func TestRS(t *testing.T) {
 	// Encode the parity set
 	err = enc.Encode(shards)
 	require.NoError(t, err)
+	t.Logf("%d", len(shards))
+	for index, shard := range shards {
+		t.Logf("index:%d %s", index, shard)
+	}
 	// Verify the parity set
 	ok, err := enc.Verify(shards)
 	require.NoError(t, err)
 	require.Equal(t, true, ok)
 	// Delete two shards
-	shards[2], shards[3] = nil, nil
+	//shards[len(shards)-1] = nil
+	//shards[len(shards)-2] = nil
+	shards[len(shards)-3] = nil
+	shards[len(shards)-4] = nil
+	shards[len(shards)-5] = nil
 
+	enc1, err := reedsolomon.New(2, 3)
+	require.NoError(t, err)
 	// Reconstruct the shards
-	err = enc.ReconstructData(shards)
+	err = enc1.Reconstruct(shards)
 	require.NoError(t, err)
 	// Verify the data set
-	ok, err = enc.Verify(shards)
+	ok, err = enc1.Verify(shards)
 	require.NoError(t, err)
 	require.Equal(t, true, ok)
+
 	t.Logf("%d", len(shards))
 	for index, shard := range shards {
 		t.Logf("index:%d %s", index, shard)
